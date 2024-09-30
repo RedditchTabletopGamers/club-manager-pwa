857f7f21a1262e933e27c97d6e70745e95a76afe
Stuart Davies <stuart.davies@junitech.com>
2024-09-30 20:40:20 +0100

    Updated front screen

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Redditch Tabletop Gamers
-        </p>
+        </h1>
+        <h2>
+          Club Manager
+        </h2>
       </header>
     </div>
   );
